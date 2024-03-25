eb50da75635dbed409e02016f08c72f916d51f65
Stefan Ciprian <stefanciprian444@gmail.com>
2024-03-25 19:21:40 +0200

    Added CipiWebs reference

--- src/components/CenterPanelComponents/Navbar.jsx
+++ src/components/CenterPanelComponents/Navbar.jsx
@@ -1,5 +1,8 @@
 import Search from "./Search";
 import { motion } from "framer-motion";
+import cipiwebsIcon from "../../assets/images/icons/cipiwebs.svg";
+
+const CipiwebsLink = "https://cipiwebs.vercel.app";
 
 export default function Navbar({
   dayWeek,
@@ -8,30 +11,42 @@ export default function Navbar({
   searchResults,
   showLoadingSearch,
   setInputValue,
-  setLocation
+  setLocation,
 }) {
   return (
     <motion.nav
       layout
       className="mb-4 p-2 gap-2 rounded-2xl bg-[#A6ADBA1A] flex flex-wrap justify-around items-center"
     >
-        <div className="sm:text-left text-center flex flex-col">
-          <h1 className="text-lg font-bold">
-            {!notLoadedForecast ? currentDay : "---"}
-          </h1>
-          <h3>{!notLoadedForecast ? dayWeek : "---"}</h3>
-        </div>
+      <div className="sm:text-left text-center flex flex-col">
+        <h1 className="text-lg font-bold">
+          {!notLoadedForecast ? currentDay : "---"}
+        </h1>
+        <h3>{!notLoadedForecast ? dayWeek : "---"}</h3>
+      </div>
 
-      <a
-        href="https://www.weatherapi.com/"
-        title="Free Weather API"
-        target="_blank"
-        rel="noreferrer"
-        className="sm:flex hidden flex-col text-left p-3 hover:bg-[#bec6d41a] transition-colors rounded-2xl"
-      >
-        <p className="text-sm">Powered by</p>
-        <p className="font-bold">WeatherAPI.com</p>
-      </a>
+      <CipiWebs />
     </motion.nav>
   );
 }
+
+const CipiWebs = () => {
+  return (
+    <a
+      href={CipiwebsLink}
+      target="_blank"
+      rel="noreferrer"
+      role="button"
+      className="flex flex-col bg-black/10 py-1 px-5 rounded-2xl shadow-sm border-[1px] border-slate-700 hover:scale-105 transition-transform"
+    >
+      <h4 className="text-left font-semibold text-text-600">Made by</h4>
+      <img
+        src={cipiwebsIcon}
+        alt="CipiWebs logo"
+        width={100}
+        height={50}
+        className="shrink-0 h-auto"
+      />
+    </a>
+  );
+};
